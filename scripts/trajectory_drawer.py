29356c1a7828dfdfb72ff4d203dd6784d4d43522
Matteo Delpini <matteo.delpini@mail.polimi.it>
2022-07-03 14:21:15 +0200

    trajectory drawer

--- scripts/trajectory_drawer.py
+++ scripts/trajectory_drawer.py
@@ -8,22 +8,84 @@
 
 
 #!/usr/bin/env python
-from http import server
 import rospy
 import numpy as np
 import cv2
+from nav_msgs.srv import GetMap
 from nav_msgs.msg import OccupancyGrid
 import os
-from robotics_project_two.srv import CreateMapAndTrajectoryImage
+from robotics_project_two.srv import CreateMapAndTrajectoryImage, ResetTrajectoryToDraw
+from geometry_msgs.msg import Pose, PoseWithCovarianceStamped
+import os
 
-def create_map_and_trajectory_image_function(req):
-    print("service called")
+cwd = os.getcwd()
 
-def main():
-    rospy.init_node('trajectory_drawer')
-    s = rospy.Service('create_map_and_trajectory_image', CreateMapAndTrajectoryImage, create_map_and_trajectory_image_function)
-    print("service is ready")
-    rospy.spin()
+class BaseMap:
+    def __init__(self,map,width,height,resolution,origin):
+        rospy.INFO("creating base map")
+        self.map = map
+        self.width = width
+        self.height = height
+        self.resolution = resolution
+        self.origin = origin
+        self.trajectory = []
+        self.poseSubscriber = rospy.Subscriber("/amcl_pose",PoseWithCovarianceStamped,self.trajectory_callback)
+        self.drawService = rospy.Service('create_map_and_trajectory_image', CreateMapAndTrajectoryImage, self.service_callback)
+        self.resetService = rospy.Service('reset_trajectory_to_draw',ResetTrajectoryToDraw,self.reset_trajectory_callback)
 
-if __name__ == "__main__":
-    main()
+    def trajectory_callback(self,data):
+        x = data.pose.pose.position.x
+        y = data.pose.pose.position.y
+        cellX = (x-self.origin.position.x)/self.resolution
+        cellY = (y-self.origin.position.x)/self.resolution
+        self.trajectory.append((cellX,cellY))
+
+    def service_callback(self,data):
+        size = (self.width,self.height)
+        image = np.zeros(size)
+
+        counter = 0
+        for p in self.map:
+            image[counter%self.width][int(counter/self.width)] = p
+            counter+=1
+        image[image == -1] = 150
+        image[image == 0] = 255
+        image[image == 100] = 0
+
+        counter = 0
+        color = (0, 255, 0)
+        for (x,y) in self.trajectory:
+            if(counter != 0):
+                cv2.line(image,previous,(x,y),color,5)
+            previous = (x,y)
+            counter+=1
+
+        image = cv2.rotate(image, cv2.ROTATE_180)
+        
+        filename = cwd+'/map_with_trajectory.png'
+        print ("saving image")
+        cv2.imwrite (filename,image)
+
+    def reset_trajectory_callback(self,data):
+        self.trajectory = []
+
+
+def save_map_callback(data):
+    rospy.INFO("salvo la mappa")
+    global baseMap
+    baseMap = BaseMap(data.data,data.info.width,data.info.height,data.info.resolution,data.info.origin)
+
+def map_listener():
+    rospy.init_node('trajectory_drawer',anonymous=False)
+    rospy.wait_for_service('static_map')
+    try:
+        get_map = rospy.ServiceProxy('static_map',GetMap)
+        map = get_map()
+        save_map_callback(map.map)
+        rospy.spin()
+    except rospy.ServiceException as e:
+        print("Service call failed: %s"%e)
+    
+
+if __name__ == '__main__':
+    map_listener()
