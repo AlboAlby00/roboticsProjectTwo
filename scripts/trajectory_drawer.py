e281d8b28f89ad5a9cd3445df7847e1e0a994ca8
AlboAlby00 <alberto.confente@mail.polimi.it>
2022-07-03 13:42:03 +0200

    service call added, still missing service code

--- scripts/trajectory_drawer.py
+++ scripts/trajectory_drawer.py
@@ -8,8 +8,22 @@
 
 
 #!/usr/bin/env python
+from http import server
 import rospy
 import numpy as np
 import cv2
 from nav_msgs.msg import OccupancyGrid
 import os
+from robotics_project_two.srv import CreateMapAndTrajectoryImage
+
+def create_map_and_trajectory_image_function(req):
+    print("service called")
+
+def main():
+    rospy.init_node('trajectory_drawer')
+    s = rospy.Service('create_map_and_trajectory_image', CreateMapAndTrajectoryImage, create_map_and_trajectory_image_function)
+    print("service is ready")
+    rospy.spin()
+
+if __name__ == "__main__":
+    main()
